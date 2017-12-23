c654cbc91caddab186f2ffb58cb9a2a92b5f19d6
Danupol Intranurux <lagseeing_0001@outlook.com>
2017-12-23 21:31:13 +0700

    Update HelloWorld.java

--- TNI-Hotel-booking-system/src/HelloWorld.java
+++ TNI-Hotel-booking-system/src/HelloWorld.java
@@ -10,6 +10,7 @@ public class HelloWorld {
 		System.out.println("Hello Pokemon2");
 		System.out.println("Hello Pokemon3");
 		System.out.println("Hello Pokemon4");
+		System.out.println("Hello Pokemon5");
 	}
 
 }
