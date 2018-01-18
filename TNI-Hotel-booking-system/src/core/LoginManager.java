19115114ec87fb13c40072c2fdbf44724f7780b1
LagSeeN <19259262+LagSeeN@users.noreply.github.com>
2018-01-18 18:45:21 +0700

    fix bux in file system

--- TNI-Hotel-booking-system/src/core/LoginManager.java
+++ TNI-Hotel-booking-system/src/core/LoginManager.java
@@ -10,12 +10,7 @@ public class LoginManager {
 
 	public static File file = new File(
 			// "D:\\git\\TNI-Hotel-booking-system\\TNI-Hotel-booking-system\\Database\\user_database.txt");
-			"C:\\Users\\Yossapon Jantarote\\git\\TNI-Hotel-booking-system\\TNI-Hotel-booking-system\\Database\\user_database.txt");
-
-	public LoginManager() {
-		this.username = "";
-		this.password = "";
-	}
+			"C:\\Users\\LagSe\\git\\TNI-Hotel-booking-system\\TNI-Hotel-booking-system\\Database\\user_database.txt");
 
 	public LoginManager(String username, String password) {
 		this.username = username;
@@ -23,7 +18,7 @@ public class LoginManager {
 	}
 
 	public String getUsername() {
-		return this.username;
+		return username;
 	}
 
 	public boolean LoginCheck() {
