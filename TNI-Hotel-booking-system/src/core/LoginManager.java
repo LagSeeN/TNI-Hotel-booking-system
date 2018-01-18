4b97eebcd3690475a4975f5cf8ae06ecee25d7f9
taifurn44 <ch.thanapat_st@tni.ac.th>
2018-01-18 12:20:14 +0700

    add file system

--- TNI-Hotel-booking-system/src/core/LoginManager.java
+++ TNI-Hotel-booking-system/src/core/LoginManager.java
@@ -5,16 +5,30 @@ import java.io.*;
 public class LoginManager {
 	private String username;
 	private String password;
-	
-	public FileReader reader = new FileReader(".\\Database\\user_database.txt");
-	
+
+	public static File file = new File(
+			"D:\\git\\TNI-Hotel-booking-system\\TNI-Hotel-booking-system\\Database\\user_database.txt");
+
 	public LoginManager() {
 		this.username = "";
 		this.password = "";
 	}
-	
+
 	public LoginManager(String username, String password) {
-		
+
 	}
 
+	public static void main(String[] args) {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+			br.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
