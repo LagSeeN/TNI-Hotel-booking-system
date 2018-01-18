3dbd30a228332a9f9b4854e2def245299ad00930
ONMITSU <ja.yossapon_st@tni.ac.th>
2018-01-18 12:21:57 +0700

    update java file

--- TNI-Hotel-booking-system/src/core/LoginManager.java
+++ TNI-Hotel-booking-system/src/core/LoginManager.java
@@ -14,7 +14,28 @@ public class LoginManager {
 	}
 	
 	public LoginManager(String username, String password) {
-		
+		this.username = username;
+		this.password = password;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	public boolean LoginCheck() { //Change in into files reader when is this system OK.
+		if (this.username != username) {
+			return false;
+		}
+		if (this.password != password) {
+			return false;
+		}
+		return true;
+	}
+	
+	public String getLoginStatus(String username,String password) {
+		if (!LoginCheck()) {
+			return "USERNAME or PASSWORD is wrong!";
+		}
+		return "";
 	}
 
 }
