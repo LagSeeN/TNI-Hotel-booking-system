7a638261cf37d8f22d33c9b8b8dbf1af928dd0cf
taifurn44 <ch.thanapat_st@tni.ac.th>
2018-01-18 11:42:50 +0700

    dump test1

--- TNI-Hotel-booking-system/src/test1.java
+++ TNI-Hotel-booking-system/src/test1.java
@@ -125,7 +125,6 @@ public class test1 {
 						int day = cal.get(Calendar.DAY_OF_MONTH);
 						int month = cal.get(Calendar.MONTH) + 1;
 						int year = cal.get(Calendar.YEAR);
-
 						int sec = cal.get(Calendar.SECOND);
 						int min = cal.get(Calendar.MINUTE);
 						int hr = cal.get(Calendar.HOUR);
