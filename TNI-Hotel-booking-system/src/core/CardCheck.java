f89944a663f7081f6a0a31ee5697069d325e03db
LagSeeN <19259262+LagSeeN@users.noreply.github.com>
2018-02-09 22:55:53 +0700

    แยกระบบบัตรเครดิตออกมา

---
+++ TNI-Hotel-booking-system/src/core/CardCheck.java
@@ -0,0 +1,55 @@
+package core;
+
+import javax.swing.JOptionPane;
+
+public class CardCheck {
+	private int[] card = new int[16];
+	private String cardnumber;
+	private String name;
+
+	public CardCheck(String name, String card) {
+		this.cardnumber = card;
+		this.name = name;
+	}
+
+	public CardCheck(String card) {
+		this.cardnumber = card;
+	}
+
+	public boolean checkCard() {
+		if (!cardnumber.startsWith("4") || cardnumber.startsWith("51") || cardnumber.startsWith("52")
+				|| cardnumber.startsWith("53") || cardnumber.startsWith("54") || cardnumber.startsWith("55"))
+			return false;
+		for (int i = 0; i < this.card.length; i++) {
+			this.card[i] = Integer.valueOf(this.cardnumber.charAt(i) - '0');
+		}
+		int sum = 0;
+		int length = this.card.length;
+		for (int i = 0; i < length; i++) {
+			int digit = this.card[length - i - 1];
+			if (i % 2 == 1) {
+				digit *= 2;
+			}
+			sum += digit > 9 ? digit - 9 : digit;
+		}
+		return sum % 10 == 0;
+	}
+
+	public boolean checkName() {
+		int space_bar = 0;
+		for (int i = 0; i < name.length(); i++)
+			if (name.charAt(i) == ' ')
+				space_bar++;
+		if (!(space_bar > 1 || space_bar == 0 || name.startsWith(" ") || name.endsWith(" ")))
+			return true;
+		if (name.isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Please enter Name", "Request Name", JOptionPane.WARNING_MESSAGE);
+			return false;
+		}
+		JOptionPane.showMessageDialog(null, "Name is incorrect" + "\nPlease try again", "Name incoorect",
+				JOptionPane.WARNING_MESSAGE);
+		return false;
+
+	}
+
+}
--- TNI-Hotel-booking-system/src/payment_credit_card_page.java
+++ TNI-Hotel-booking-system/src/payment_credit_card_page.java
@@ -10,6 +10,8 @@ import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 
+import core.CardCheck;
+
 import java.awt.Font;
 import javax.swing.JPanel;
 import java.awt.Color;
@@ -17,8 +19,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.List;
 import java.awt.Toolkit;
 
 public class payment_credit_card_page {
@@ -71,8 +71,8 @@ public class payment_credit_card_page {
 	private void initialize() {
 
 		frmCreditCardPayment = new JFrame();
-		frmCreditCardPayment
-				.setIconImage(Toolkit.getDefaultToolkit().getImage(payment_credit_card_page.class.getResource("/img/icon1.png")));
+		frmCreditCardPayment.setIconImage(
+				Toolkit.getDefaultToolkit().getImage(payment_credit_card_page.class.getResource("/img/icon1.png")));
 		frmCreditCardPayment.setResizable(false);
 		frmCreditCardPayment.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		frmCreditCardPayment.setTitle("Payment (Credit Card) | Hotel Booking System");
@@ -113,7 +113,8 @@ public class payment_credit_card_page {
 			@Override
 			public void keyReleased(KeyEvent evt) {
 				if (CardNumber.getText().length() == 16) {
-					if (!(checkCard(CardNumber.getText()))) {
+					CardCheck card = new CardCheck(CardNumber.getText());
+					if (!(card.checkCard())) {
 						CardNumber.setBackground(new Color(255, 0, 51));
 						lblCardError.setVisible(true);
 						btnPay.setEnabled(false);
@@ -126,7 +127,7 @@ public class payment_credit_card_page {
 				if (CardNumber.getText().length() < 16) {
 					CardNumber.setBackground(new Color(255, 255, 255));
 					lblCardError.setVisible(false);
-					btnPay.setEnabled(true);
+					btnPay.setEnabled(false);
 				}
 				if (CardNumber.getText().startsWith(""))
 					lblCardicon.setVisible(false);
@@ -173,35 +174,17 @@ public class payment_credit_card_page {
 		btnPay.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				// Pay
-				if (Name.getText().isEmpty() || CardNumber.getText().isEmpty()) {
-					if (CardNumber.getText().isEmpty())
-						JOptionPane.showMessageDialog(null, "Please enter Card", "Request Card",
-								JOptionPane.WARNING_MESSAGE);
-					else if (Name.getText().isEmpty())
-						JOptionPane.showMessageDialog(null, "Please enter Name", "Request Name",
-								JOptionPane.WARNING_MESSAGE);
-					else
-						JOptionPane.showMessageDialog(null, "Please enter Name or Card", "Request Name or Card",
-								JOptionPane.WARNING_MESSAGE);
-				} else if (!(checkName(Name.getText()))) {
-					JOptionPane.showMessageDialog(null, "Name is incorrect" + "\nPlease try again", "Name incoorect",
-							JOptionPane.WARNING_MESSAGE);
-					Name.setText(null);
-				} else if (!CardNumber.getText().startsWith("4") || CardNumber.getText().startsWith("51")
-						|| CardNumber.getText().startsWith("52") || CardNumber.getText().startsWith("53")
-						|| CardNumber.getText().startsWith("54") || CardNumber.getText().startsWith("55")) {
-					JOptionPane.showMessageDialog(null, "Card type wrong");
-					CardNumber.setText("");
-					CardNumber.setBackground(new Color(255, 255, 255));
-
-				} else
+				CardCheck name = new CardCheck(Name.getText(), CardNumber.getText());
+				if (name.checkName()) {
 					JOptionPane.showConfirmDialog(null,
 							"Total Price : " + (total) + "\nPlease continue at EDC machine.\n\nPayment success?",
 							"Please continue at EDC machine", JOptionPane.YES_NO_OPTION,
 							JOptionPane.INFORMATION_MESSAGE);
+				}
 			}
 		});
 		btnPay.setBounds(259, 90, 130, 27);
+		btnPay.setEnabled(false);
 		card_input.add(btnPay);
 		btnPay.setFont(new Font("Tahoma", Font.PLAIN, 18));
 
@@ -244,47 +227,4 @@ public class payment_credit_card_page {
 		card_input.add(lblCardError);
 	}
 
-	public boolean checkName(String name) {
-		int space_bar = 0;
-		for (int i = 0; i < name.length(); i++)
-			if (name.charAt(i) == ' ')
-				space_bar++;
-		return (space_bar > 1 || space_bar == 0 || name.startsWith(" ") || name.endsWith(" ")) ? false : true;
-	}
-
-	public boolean checkCard(String text_digits) {
-		List<Integer> digits = new ArrayList<Integer>();
-		for (int i = 0; i < text_digits.length(); i++) {
-			char c = text_digits.charAt(i);
-			digits.add(Integer.valueOf(c - '0'));
-		}
-
-		int[] card = new int[digits.size()];
-		for (int i = 0; i < card.length; i++) {
-			card[i] = Integer.valueOf(digits.get(i));
-		}
-
-		int sum = 0;
-		int length = card.length;
-		for (int i = 0; i < length; i++) {
-
-			// รับเลขบัตนจากหลังสุดมาหน้าสุด เช่น4024 0071 3481 3392 ก็จะเอา 2
-			// มาคิดก่อน
-			int digit = card[length - i - 1];
-
-			// เลขหลักคู่เอาไปคูณ2
-			if (i % 2 == 1) {
-				digit *= 2;
-			}
-			// สูตรหาเลข sum ถ้ามากกว่า 9 เอาไปลบ 9
-			// ตัวอย่าง
-			// 9*2 = 18 ถ้าคิดมือคือ แยก 1 กับ 9 ออก จะเป็น 1+9 = 10
-			// ระบบคอมมันทำไม่ได้ เลยเอา 9*2 = 18 แล้วเอาไป ลบ 9 จะได้ 10 พอดี
-			// ตัวอย่างอื่นๆก็มี เช่น 7*2 = 14 = 1+4 = 5 ระบบคอมคิด 7*2 = 14 =
-			// 14-9 = 5
-			sum += digit > 9 ? digit - 9 : digit;
-		}
-		// ถ้าหาร 10 ลงตัวแปลว่าเป็นบัตรแท้
-		return sum % 10 == 0;
-	}
 }
